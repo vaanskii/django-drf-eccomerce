f904e67e69434990afbe505428afca74ee99a42b
Giorgi Vanadze <giorgivanadze03@gmail.com>
2024-08-24 21:03:06 +0400

    Integrate views and serializers. - Add list methods to Category, Brand, and Product viewsets with schema documentation. - Create serializers for Category, Brand, and Product models with nested relationships.

--- drfecommerce/drfecommerce/product/serializers.py
+++ drfecommerce/drfecommerce/product/serializers.py
@@ -4,14 +4,17 @@ from .models import *
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ['id', 'name', 'parent']
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        fields = "__all__"
+        fields = ['id','name']
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    brand = BrandSerializer()
+    
     class Meta:
         model = Product
         fields = "__all__"
--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Brand, Product
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 from drf_spectacular.utils import extend_schema
 
 class CategoryViewSet(viewsets.ViewSet):
@@ -11,4 +11,22 @@ class CategoryViewSet(viewsets.ViewSet):
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+    
+
+class ProductViewSet(viewsets.ViewSet):
+    queryset = Product.objects.all()
+
+    @extend_schema(responses=ProductSerializer)
+    def list(self, request):
+        serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
