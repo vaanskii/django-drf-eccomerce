8297816df4183140492aaf58b5e6764dd33e1e93
Giorgi Vanadze <giorgivanadze03@gmail.com>
2024-08-24 14:20:28 +0400

    Integrate Category view and urls. - Integrate CategoryViewSet which is returning all categoryes. - Integrate category viewset url.

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ViewSet):
+    queryset = Category.objects.all()
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
